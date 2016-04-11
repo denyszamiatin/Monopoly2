d2296ee8cbec51d51d68b8b4c144263e3f168164
yu-shvets <yukhimov12345@gmail.com>
2016-04-11 17:14:37 +0300

    class Player

--- monopoly.py
+++ monopoly.py
@@ -4,11 +4,9 @@ import random
 def roll_a_dice():
     """
     Function to get the result of rolling two dice
-    :return: tuple of integers as a result of rolling two dice
+    :return: list of integers as a result of rolling two dice
     """
-    first = random.randint(1, 6)
-    second = random.randint(1, 6)
-    return (first, second)
+    return random.sample(range(1, 7), 2)
 
 
 class Player:
