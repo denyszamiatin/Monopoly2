38d22344578c12a050bcf691b2d9fdfe4b46df7c
yu-shvets <yukhimov12345@gmail.com>
2016-04-11 16:51:21 +0300

    Function to get the result of rolling two dice

--- monopoly.py
+++ monopoly.py
@@ -1,1 +1,12 @@
 import random
+
+
+def roll_a_dice():
+    """
+    Function to get the result of rolling two dice
+    :return: tuple of integers as a result of rolling two dice
+    """
+    first = random.randint(1, 6)
+    second = random.randint(1, 6)
+    return (first, second)
+
