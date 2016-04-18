753a20ab2741f6dd092960e9ecd598b934f38066
YuriyLyashko <yu.i.lyashko@gmail.com>
2016-04-18 12:16:06 +0300

    add  class Field and fields to class Field

--- monopoly.py
+++ monopoly.py
@@ -53,48 +53,58 @@ class CollectionPlayers:
         ]
 
 class Field:
-    fields = [
-        'Collect $200 salary as you pass GO',
-        'Mediter-ranean Avenue $60',
+    '''
+    Field
+    '''
+    __fields = [
+        ['Collect $200 salary as you pass GO', 200],
+        ['Mediter-ranean Avenue $60', 'brown', -60],
         'Community Chest',
-        'Baltic Avenue $60',
-        'Income Tax (pay $200)',
-        'Reading Railroad $200',
-        'Oriental Avenue $100',
+        ['Baltic Avenue $60', 'brown', -60],
+        ['Income Tax (pay $200)', -200],
+        ['Reading Railroad $200', -200],
+        ['Oriental Avenue $100', 'light blue', -100],
         'Chance',
-        'Vermont Avenue $100',
-        'Connecticut Avenue $120',
+        ['Vermont Avenue $100', 'light blue', -100],
+        ['Connecticut Avenue $120', 'light blue', -120],
         'In Jail/Just Visiting',
-        'St. Charles Place$140',
-        'Electric Company $150',
-        'States Avenue $140',
-        'Virginia Avenue $160',
-        'Pennsylvania Railroad $200',
-        'St. James Place $180',
+        ['St. Charles Place$140', 'pink', -140],
+        ['Electric Company $150', -150],
+        ['States Avenue $140', 'pink', -140],
+        ['Virginia Avenue $160', 'pink', -160],
+        ['Pennsylvania Railroad $200', -200],
+        ['St. James Place $180', 'orange', -180],
         'Community Chest',
-        'Tennessee Avenue $180',
-        'New York Avenue $200',
+        ['Tennessee Avenue $180', 'orange', -180],
+        ['New York Avenue $200', 'orange', -200],
         'Free Parking',
-        'Kentucky Avenue $220',
+        ['Kentucky Avenue $220', 'red', -220],
         'Chance',
-        'Indiana Avenue $220',
-        'Illinois Avenue $240',
-        'B&O Railroad $200',
-        'Atlantic Avenue $260',
-        'Ventnor Avenue $260',
-        'Water Works $150',
-        'Marvin Gardens $280',
+        ['Indiana Avenue $220',  'red', -220],
+        ['Illinois Avenue $240', 'red', -240],
+        ['B&O Railroad $200', -200],
+        ['Atlantic Avenue $260', 'yellow', -260],
+        ['Ventnor Avenue $260', 'yellow', -260],
+        ['Water Works $150', -150],
+        ['Marvin Gardens $280', 'yellow', -280],
         'Go To Jail',
-        'Pacific Avenue $300',
-        'North Carolina Avenue $300',
+        ['Pacific Avenue $300', 'green', -300],
+        ['North Carolina Avenue $300', 'green', -300],
         'Community Chest',
-        'Pennsylvania Avenue $320',
-        'Short Line $200',
+        ['Pennsylvania Avenue $320', 'green', -320],
+        ['Short Line $200', -200],
         'Chance',
-        'Park Place $350',
-        'Luxury Tax (pay $100)',
-        'Boardwalk $400'
+        ['Park Place $350', 'blue', -350],
+        ['Luxury Tax (pay $100)', -100],
+        ['Boardwalk $400', 'blue', -350]
     ]
+    def __init__(self, number):
+        '''
+        :param number: number field 0 - 39
+        '''
+        self.field = self.__fields[number]
+
+
 
 if __name__ == "__main__":
     import doctest
